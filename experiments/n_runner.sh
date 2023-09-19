f0fc18a75c881ec21316c3682b1b9076d26f08fe
Dawson Brown <dawsonstbrown@gmail.com>
2023-09-19 17:16:59 -0400

    changes

--- experiments/n_runner.sh
+++ experiments/n_runner.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# for i in {1..}
-# do
-#     echo "Doing batch: $i"
-#     # echo "y
-#     # o" | ./src/thesis/explore.py --all --benchmark-dir ../benchmarks-asai
-# done
-
 for i in $( seq 1 $1 )
 do
     echo "Doing batch: $i"
-    echo "y
-    o" | ./src/thesis/explore.py --all --benchmark-dir ../benchmarks-tiny
+    ./runner.sh $2
 done
---
+++ experiments/runner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "y
+o" | ./src/thesis/explore.py --all --benchmark-dir ../benchmarks-$1
